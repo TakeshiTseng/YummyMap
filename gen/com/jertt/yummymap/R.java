c671ddf86315fd0877d700ad9e44ce6f035e8c7b
Taeshi <a86487817@gmail.com>
2013-07-04 22:24:57 +0800

    add titles

--- gen/com/jertt/yummymap/R.java
+++ gen/com/jertt/yummymap/R.java
@@ -38,6 +38,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int title_achievement=0x7f050008;
+        public static final int title_around=0x7f050005;
+        public static final int title_favorite=0x7f050007;
+        public static final int title_map=0x7f050006;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int title_section3=0x7f050004;
