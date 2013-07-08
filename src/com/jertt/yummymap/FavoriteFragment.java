7cc0be36f9aa9a7f12d4f52d28454d45080a15e4
jeffrey810702 <a07021217@hotmail.com>
2013-07-09 00:04:47 +0800

    favorite

--- src/com/jertt/yummymap/FavoriteFragment.java
+++ src/com/jertt/yummymap/FavoriteFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class FavoriteFragment extends Fragment{
+	ListView favorite_listview;
+
 	public FavoriteFragment() {
 		// TODO Auto-generated constructor stub
 	}
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -18,7 +20,8 @@ public class FavoriteFragment extends Fragment{
 				container, false);		
 		// TODO 初始化
 		// TODO 用 getArguments().getxxx 來取得傳過來的參數
-		setContentView(R.layout.favorite_fragment);
+		favorite_listview = (ListView) rootView.findViewById(R.layout.favorite_fragment);
+		
 		return rootView;
 	}
 }
