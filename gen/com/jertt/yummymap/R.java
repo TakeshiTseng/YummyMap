5eb0ad17025401e064a1205b4017e2c0914e7b1e
Jeff <Jeff@Jeff-PC>
2013-07-12 20:07:47 +0800

    favoritefragment

--- gen/com/jertt/yummymap/R.java
+++ gen/com/jertt/yummymap/R.java
@@ -24,22 +24,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int linearLayout1=0x7f080002;
+        public static final int LinearLayout1=0x7f080006;
+        public static final int action_settings=0x7f080009;
+        public static final int imageView1=0x7f080003;
+        public static final int listView1=0x7f080005;
         public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080005;
+        public static final int section_label=0x7f080007;
         public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080003;
+        public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080004;
-        public static final int webView1=0x7f080006;
+        public static final int webView1=0x7f080008;
     }
     public static final class layout {
         public static final int achievement_fragment=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int around_fragment=0x7f030002;
-        public static final int favorite_fragment=0x7f030003;
-        public static final int fragment_main_dummy=0x7f030004;
-        public static final int map_fragment=0x7f030005;
+        public static final int around_listview=0x7f030003;
+        public static final int favorite_fragment=0x7f030004;
+        public static final int favorite_listview=0x7f030005;
+        public static final int fragment_main_dummy=0x7f030006;
+        public static final int map_fragment=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
